cf4fa999df27df7c396be795a7881ece0ca8a43b
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2024-03-10 22:48:00 +0530

    Commit By FR

--- mfile.py
+++ mfile.py
@@ -7,13 +7,18 @@
 
 a = 44  # Global  Variable
 
+# def fun1():
+#     global a
+#     print(f"Global variable value : - {a}")
+#     a = 20  # Local Variable when global variable is not used
+#     print(f"Local Variable value is : {a}")
+#
+#
+# fun1()
+# print(f"The value of a defined outside function is : - {a}")
 
-def fun1():
-    global a
-    print(f"Global variable value : - {a}")
-    a = 20  # Local Variable when global variable is not used
-    print(f"Local Variable value is : {a}")
+"""Enumerate Function """
+lst = ['farhad',20.2,False,22]
 
-
-fun1()
-print(f"The value of a defined outside function is : - {a}")
+for i in lst:
+    print(i)
